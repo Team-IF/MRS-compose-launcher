2ad62426bf4d27d5d518862ede685cfa7563b2be
waixxt3213 <qnqn60360@gmail.com>
2020-12-24 22:35:41 +0900

    copy template

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,20 +1,37 @@
 import androidx.compose.desktop.Window
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.getValue
+import androidx.compose.material.TextField
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.*
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.input.key.shortcuts
 
-fun main() = Window {
-    var text by remember { mutableStateOf("Hello, World!") }
-
+@OptIn(ExperimentalKeyInput::class)
+fun main() = Window(title = "MRS-compose-launcher", size = IntSize(1280, 720)) {
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        var consumedText by remember { mutableStateOf(0) }
+        var text by remember { mutableStateOf("") }
+        var text2 by remember { mutableStateOf("") }
+        Column(Modifier.fillMaxSize(), Arrangement.spacedBy(7.dp)) {
+            Text("Test Text")
+            TextField(
+                value = text,
+                onValueChange = { text = it },
+            )
+            Text("Test Text2")
+            TextField(
+                value = text2,
+                onValueChange = { text2 = it },
+            )
         }
     }
 }
